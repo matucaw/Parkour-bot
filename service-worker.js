0e4397e4c5fb965518e3cd7276dff37c78e4dc0f
GameWeb <matucazinho@gmail.com>
2025-08-24 16:04:53 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,18 @@
+// Força instalação do novo service worker
 self.addEventListener('install', event => {
   console.log('Service Worker instalado!');
+  self.skipWaiting(); // ativa imediatamente o novo SW
 });
 
+// Ativa o service worker imediatamente em todas as abas
+self.addEventListener('activate', event => {
+  console.log('Service Worker ativado!');
+  event.waitUntil(clients.claim()); // garante que todos os clientes usem o novo SW
+});
+
+// Intercepta requisições e força sempre buscar a versão mais nova
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
